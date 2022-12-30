9c4ad683e95c4aaafbda40aad5660340fc738c97
AdrianChacon98 <adrian.chacon22@hotmail.com>
2022-12-30 16:11:52 -0600

    first commit

---
+++ src/main/java/algorithms/coretopics/CoreTopicsApplication.java
@@ -0,0 +1,13 @@
+package algorithms.coretopics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreTopicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreTopicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/algorithms/coretopics/CoreTopicsApplicationTests.java
@@ -0,0 +1,13 @@
+package algorithms.coretopics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreTopicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
